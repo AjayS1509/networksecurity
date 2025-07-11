01bf6c5153512e88eca6756f9af930344ad1abc0
Ajay Soni <ajaypsoni1509@gmail.com>
2025-07-11 18:12:17 +0530

    Data ingestion component created

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -1,17 +1,17 @@
-import sys
-from networksecurity.logging import logger
+# import sys
+# from networksecurity.logging import logger
 
-class NetworkSecurityException(Exception):
-    def __init__(self, error_message,error_detail:sys): # type: ignore
-        self.error_message = error_message
-        _,_,exc_tb = error_detail.exc_info()
+# class NetworkSecurityException(Exception):
+#     def __init__(self, error_message,error_detail:sys):
+#         self.error_message = error_message
+#         _,_,exc_tb = error_detail.exc_info()
 
-        self.lineno = exc_tb.tb_lineno # type: ignore
-        self.file_name = exc_tb.tb_frame.f_code.co_filename # type: ignore
+#         self.lineno = exc_tb.tb_lineno
+#         self.file_name = exc_tb.tb_frame.f_code.co_filename
 
 
-    def __str__(self) :
-        return "Errror occured in python script name [{0}] line number [{1}] error message [{2}]".format(self.file_name,self.lineno,str(self.error_message))
+#     def __str__(self) :
+#         return "Errror occured in python script name [{0}] line number [{1}] error message [{2}]".format(self.file_name,self.lineno,str(self.error_message))
     
 
 # if __name__=='__main__':
@@ -21,3 +21,28 @@ class NetworkSecurityException(Exception):
 #         print("this will not be printted",a)
 #     except Exception as e:
 #         raise NetworkSecurityException(e,sys)
+
+
+import sys
+import traceback
+from networksecurity.logging import logger  # Optional if not used
+
+class NetworkSecurityException(Exception):
+    def __init__(self, error_message, error_detail):
+        super().__init__(error_message)
+        self.error_message = error_message
+
+        _, _, exc_tb = error_detail
+        if exc_tb:
+            self.lineno = exc_tb.tb_lineno
+            self.file_name = exc_tb.tb_frame.f_code.co_filename
+        else:
+            self.lineno = None
+            self.file_name = None
+
+    def __str__(self):
+        return "Error occurred in Python script [{0}] at line [{1}]: {2}".format(
+            self.file_name if self.file_name else "Unknown",
+            self.lineno if self.lineno else "Unknown",
+            str(self.error_message)
+        )
